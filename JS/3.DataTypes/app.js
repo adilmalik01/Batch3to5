fc62d062eae6caee40a990cd110e9891e28e02f2
adilmalik01 <malikadil5679@gmail.com>
2026-01-24 16:17:55 +0500

    variable & datatypes

---
+++ JS/3.DataTypes/app.js
@@ -0,0 +1,53 @@
+// String  
+
+// "" , 'asdasd 345678 true bolean' , `asdsad`
+console.log("Adil Malik");
+
+// number 
+console.log(1234567890);
+
+// Boolean 
+console.log(false);
+
+
+// null
+console.log(null);
+
+// undefined
+console.log(undefined);
+
+
+
+// Array
+// Object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS/4.Variables/app.js
@@ -0,0 +1,91 @@
+
+// var 
+// let 
+// const 
+
+
+// var a = "Adil"
+
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+
+
+
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+// console.log("Malik");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num = 10
+
+// console.log(num);
+
+
+// num = 20
+
+// console.log(num);
+
+
+
+
+
+// let num = 10
+// let num = 20
+
+// console.log(num);
+
+
+const username = "Adil"
+// username = "Adil Malik"
+console.log(username);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
