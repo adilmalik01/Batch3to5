d81a11fd03c1412e926fe65014fdd120a2c1debf
adilmalik01 <malikadil5679@gmail.com>
2026-01-27 16:10:52 +0500

    comparision operator complete

---
+++ JS/5.Operator/app.js
@@ -0,0 +1,78 @@
+// Arithmetic Operator 
+// + - / * 
+
+// let num1 = 10
+// let num2 = 5
+// let sum = num1 - num2
+
+// console.log(sum * 3);
+
+
+// Assignment Operator
+// += -= /= 
+// let num1 = 10
+// let num2 = 5
+
+// let sum = num1 + num2
+
+// sum += 5
+
+
+// console.log(sum);
+
+
+
+// Comparision Operator
+// ==  ===   >   <   >=    <=  !
+
+
+// let num1 = 10
+// let num2 = 9
+
+// console.log(num1 != num2);
+
+
+
+
+
+
+
+// Logical Operator
+// &&    ||
+
+
+let email = "adilmalik@gmail.com"
+let password = 1234
+
+let verify = email == "adilmalik@gmail.com" || password == 12345
+console.log(verify);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
