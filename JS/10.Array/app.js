7f44165cf2e7bd5c3721388ab36f279c18e664f3
adilmalik01 <malikadil5679@gmail.com>
2026-02-10 15:59:34 +0500

    update:Array & Array Method Complete

---
+++ JS/10.Array/app.js
@@ -0,0 +1,52 @@
+
+// let student = "Adil Malik"
+// console.log(student);
+
+
+
+let students = ["Adil", "Ali", "Hamza"]
+console.log(students);
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS/11.ArrayMethods/app.js
@@ -0,0 +1,78 @@
+
+
+
+// let students = ["Adil", "Ali", "Hamza","Taha","Aqib","Yasir"]
+// console.log(students);
+
+
+
+// push 
+// students.push("Aqib","Yasir")
+// console.log(students);
+
+
+// pop 
+// students.pop()
+// students.pop()
+// students.pop()
+
+// console.log(students);
+
+
+
+// shift 
+// students.shift()
+// console.log(students);
+
+// unshift 
+// students.unshift("Hello")
+// console.log(students);
+
+let students = ["Adil", "Ali", "Hamza", "Taha", "Aqib", "Yasir"]
+console.log(students);
+
+// slice 
+// let copyArray = students.slice(1, 5)
+// console.log(copyArray);
+
+// splice 
+students.splice(2,1,"New")
+console.log(students);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
