4e7e38f51c2621b259198d4de9aba56478b78114
adilmalik01 <malikadil5679@gmail.com>
2026-02-12 16:14:28 +0500

    update: Object

---
+++ JS/12.Object/app.js
@@ -0,0 +1,133 @@
+
+// let studentArray = ["Adil", 19, 10, true]
+
+
+// let studentObject = {
+//     name: "Adil",
+//     age: 19,
+//     class: 10,
+//     isActive: true,
+//     subjects: ["Urdu", "English", "Math"]
+// }
+
+// console.log(studentObject);
+
+
+
+// [
+
+//     {
+//         name: "Adil",
+//         age: 19,
+//         class: 10,
+//         isActive: true
+//     },
+//     {
+//         name: "Adil",
+//         age: 19,
+//         class: 10,
+//         isActive: true
+//     }
+
+// ]
+
+
+
+
+
+// let students = [
+//     {
+//         name: "Adil",
+//         age: 19,
+//         class: 12,
+//         isActive: true,
+//         marks: {
+//             english: 90,
+//             urdu: 100
+//         }
+//     },
+//     {
+//         name: "Ali",
+//         age: 20,
+//         class: 10,
+//         isActive: false,
+//         marks: {
+//             english: 30,
+//             urdu: 40
+//         }
+//     },
+//     {
+//         name: "Hamza",
+//         age: 14,
+//         class: 6,
+//         isActive: true,
+//         marks: {
+//             english: 10,
+//             urdu: 10
+//         }
+//     },
+//     {
+//         name: "Aqib",
+//         age: 20,
+//         class: 12,
+//         isActive: false,
+//         marks: {
+//             english: 30,
+//             urdu: 40
+//         }
+//     }
+// ]
+
+
+// console.log(students[3].marks.urdu);
+
+
+
+
+// let weatherData = {
+//     "coord": {
+//         "lon": 67.0822,
+//         "lat": 24.9056
+//     },
+//     "weather": [
+//         {
+//             "id": 803,
+//             "main": "Clouds",
+//             "description": "broken clouds",
+//             "icon": "04d"
+//         }
+//     ],
+//     "base": "stations",
+//     "main": {
+//         "temp": 30.9,
+//         "feels_like": 28.85,
+//         "temp_min": 30.9,
+//         "temp_max": 30.9,
+//         "pressure": 1016,
+//         "humidity": 14,
+//         "sea_level": 1016,
+//         "grnd_level": 1012
+//     },
+//     "visibility": 7000,
+//     "wind": {
+//         "speed": 2.57,
+//         "deg": 20
+//     },
+//     "clouds": {
+//         "all": 71
+//     },
+//     "dt": 1770893766,
+//     "sys": {
+//         "type": 1,
+//         "id": 7576,
+//         "country": "PK",
+//         "sunrise": 1770862101,
+//         "sunset": 1770902618
+//     },
+//     "timezone": 18000,
+//     "id": 1174872,
+//     "name": "Karachi",
+//     "cod": 200
+// }
+
+// console.log(weatherData.main);
