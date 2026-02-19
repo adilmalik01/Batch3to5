be8a662b286e2335d8a79862e8806624a8ad09f5
adilmalik01 <malikadil5679@gmail.com>
2026-02-19 13:11:13 +0500

    update : loop & table

---
+++ JS/13.Loops/app.js
@@ -0,0 +1,79 @@
+
+// LOOPS 
+
+// FOR 
+// WHILE 
+// DO WHILE 
+
+
+
+// ES6  ecmascript 6
+// FOREACH 
+// MAP 
+// FOR IN 
+// FOR OF 
+
+
+// let students = ["Adil", "Ali", "Hamza"]
+// console.log(students);
+
+// let students = [
+//     {
+//         name: "Adil",
+//         age: 19,
+//         class: 12,
+//         isActive: true,
+//         marks: {
+//             english: 90,
+//             urdu: 100
+//         }
+//     },
+//     {
+//         name: "Ali",
+//         age: 20,
+//         class: 10,
+//         isActive: false,
+//         marks: {
+//             english: 30,
+//             urdu: 40
+//         }
+//     },
+//     {
+//         name: "Hamza",
+//         age: 14,
+//         class: 6,
+//         isActive: true,
+//         marks: {
+//             english: 10,
+//             urdu: 10
+//         }
+//     },
+//     {
+//         name: "Aqib",
+//         age: 20,
+//         class: 12,
+//         isActive: false,
+//         marks: {
+//             english: 30,
+//             urdu: 40
+//         }
+//     }
+// ]
+
+
+
+let userInput = prompt("Enter your table number !")
+
+
+
+for (let i = 1; i <= 10; i++) {
+
+    console.log(`${userInput} x ${i} =  ${userInput * i}`);
+
+    // console.log(2 + "x" + i + "=" + 2 * i);
+
+}
+
+
+
+
